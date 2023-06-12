ffc1f6b14fbb6054a0d40ca1dd6309fbe2de8eb7
Bruno Lemgruber <brunoteixeiralc@gmail.com>
2023-06-12 17:08:10 -0300

    first commit

---
+++ teste.py
@@ -0,0 +1,41 @@
+import requests
+import pandas as pd
+
+url = "https://api.linximpulse.com/engage/search/v3/hotsites"
+
+querystring = {"apikey":"paodeacucar","origin":"https://www.paodeacucar.com","page":"1","resultsPerPage":"12","name":"semanacompleta_cervejasespeciais","salesChannel":["461","catalogmkp"],"sortBy":"descDiscount","filter":"d:3718:3719"}
+
+payload = ""
+headers = {
+    "authority": "api.linximpulse.com",
+    "accept": "application/json, text/plain, */*",
+    "accept-language": "en-US,en;q=0.9",
+    "if-none-match": "W/16a87-CtUShW36pU8cJ5TRJn7iqnI6HsY",
+    "origin": "https://www.paodeacucar.com",
+    "referer": "https://www.paodeacucar.com/",
+    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+}
+
+response = requests.request("GET", url, data=payload, headers=headers, params=querystring)
+
+json_res = response.json()
+
+res = []
+
+# print(json_res["products"])
+
+for i in json_res["products"]:
+    discount = "Same price"
+    if "desconto" in i["skus"][0]["specs"]:
+        discount = i["skus"][0]["specs"]["desconto"][0]
+        
+    res.append({"name": i["name"],
+                "discount": discount,
+                "price": i["price"],
+                "old_price": i["oldPrice"],
+                "status": i["status"],
+                "url_item": i["url"]})
+    
+df = pd.json_normalize(res)
+df = pd.DataFrame(df) 
+df.to_csv("pao_acucar.csv", encoding="utf-8", index=False, sep=";")   
