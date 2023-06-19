1deb8880b3d8eea4107a4c2b6947efb6aa976498
Bruno Lemgruber <brunoteixeiralc@gmail.com>
2023-06-19 14:28:41 -0300

    telegram bot

--- ws_pda.py
+++ ws_pda.py
@@ -1,12 +1,15 @@
 import requests
 import base64
 import pandas as pd
+import locale
 import sendgrid
 from sendgrid.helpers.mail import (
     Mail, Content, To, Email, Attachment, FileContent, FileName,
     FileType, Disposition)
 from io import BytesIO
 from decouple import config
+
+locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
 
 url = "https://api.linximpulse.com/engage/search/v3/hotsites"
 
@@ -47,9 +50,9 @@ df.to_csv("desc_pda_cerva.csv", encoding="utf-8", index=False, sep=";")
 
 def send_email(res):
     
-    contentEmailBody1 = res[0]["name"] + " , Preço com desc: " + str(res[0]["price"]) + " , Preço normal: " + str(res[0]["old_price"])
-    contentEmailBody2 = res[1]["name"] + " , Preço com desc: " + str(res[1]["price"]) + " , Preço normal: " + str(res[1]["old_price"])
-    contentEmailBody3 = res[2]["name"] + " , Preço com desc: " + str(res[2]["price"]) + " , Preço normal: " + str(res[2]["old_price"])
+    contentEmailBody1 = res[0]["name"] + " , Preço com desc: " + locale.currency(res[0]["price"], grouping=True, symbol=False) + " , Preço normal: " + locale.currency(res[0]["old_price"], grouping=True, symbol=False)
+    contentEmailBody2 = res[1]["name"] + " , Preço com desc: " + locale.currency(res[1]["price"], grouping=True, symbol=False) + " , Preço normal: " + locale.currency(res[1]["old_price"], grouping=True, symbol=False)
+    contentEmailBody3 = res[2]["name"] + " , Preço com desc: " + locale.currency(res[2]["price"], grouping=True, symbol=False) + " , Preço normal: " + locale.currency(res[2]["old_price"], grouping=True, symbol=False)
     
     buffer = BytesIO()
     df.to_csv(buffer);
@@ -70,7 +73,7 @@ def send_email(res):
     content = Content("text/plain", "Segue em anexo as 12 cervejas artesanais com maior desconto no PDA\n\n" + "TOP 3 DE DESCONTO\n\n" +
                       str(contentEmailBody1) + 
                       "\n" + str(contentEmailBody2) + 
-                      "\n" + str(contentEmailBody3)
+                      "\n" + str(contentEmailBody3) + "\n"
                       ) 
     mail = Mail(from_email, to_email, subject, content)
     mail.attachment = attachment
@@ -84,3 +87,35 @@ def send_email(res):
     print('Email enviado')
     
 send_email(res)
+
+def send_to_telegram(message):
+
+    apiToken = config('API_TELEGRAM')
+    chatID = config('CHAT_ID_TELEGRAM')
+    apiURL = f'https://api.telegram.org/bot{apiToken}/sendMessage'
+
+    try:
+        response = requests.post(apiURL, json={'chat_id': chatID, 'text': "🍺 " + res[0]["name"] + 
+                                               "\nPreço com desc: " + "💰 " + locale.currency(res[0]["price"], grouping=True, symbol=False) + 
+                                               "\nPreço normal: " + "💰 " + locale.currency(res[0]["old_price"], grouping=True, symbol=False) + 
+                                               "\nLink: " + res[0]["url_item"]}
+                                 )
+        print(response.text)
+        response = requests.post(apiURL, json={'chat_id': chatID, 'text': "🍺 " + res[1]["name"] + 
+                                               "\nPreço com desc: " + "💰 " + locale.currency(res[1]["price"], grouping=True, symbol=False) + 
+                                               "\nPreço normal: " + "💰 " + locale.currency(res[1]["old_price"], grouping=True, symbol=False) + 
+                                              "\nLink: " + res[1]["url_item"]}
+                                 )
+        print(response.text)
+        response = requests.post(apiURL, json={'chat_id': chatID, 'text': "🍺 " + res[2]["name"] + 
+                                               "\nPreço com desc: " + "💰 " + locale.currency(res[2]["price"], grouping=True, symbol=False) + 
+                                               "\nPreço normal: " + "💰 " + locale.currency(res[2]["old_price"], grouping=True, symbol=False) + 
+                                               "\nLink: " + res[2]["url_item"]}
+                                 )
+        print(response.text)
+    except Exception as e:
+        print(e)
+        
+    print('Mensagens enviadas')
+
+send_to_telegram(res)
